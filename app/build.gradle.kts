1750f6952c09574e6cd8cf14a907d34475923335
Ignacio Ampurdanes <ignaciopercusion@gmail.com>
2025-02-19 19:11:59 +0100

    seguimos con el probelma de los nombre en la activitu¡y editar, pero he modificado la distancia de los botones Save y Cancel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,10 +68,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    dependencies {
-        // ... otras dependencias ...
-        implementation("com.google.android.material:material:1.11.0") // Asegúrate de usar la versión más reciente
-        // ... otras dependencias ...
-    }
+    implementation("com.google.android.material:material:1.11.0")
+
+
 
 }
